2ed23cfd174606aafff0a54f9b79fd1b07cc9662
Luiz Felipe Isobata Yoshiassu <luiz.isobata@gmail.com>
2024-02-07 16:52:42 -0300

    Veiculo Entity e Resource

---
+++ src/main/java/com/desafiobackendjr/desafio/entities/Veiculo.java
@@ -0,0 +1,77 @@
+package com.desafiobackendjr.desafio.entities;
+
+import java.io.Serializable;
+
+public class Veiculo implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	private String proprietario;
+	private String cpf;
+	private String placa;
+	
+	public Veiculo() {
+	}
+
+	public Veiculo(String proprietario, String cpf, String placa) {
+		super();
+		this.proprietario = proprietario;
+		this.cpf = cpf;
+		this.placa = placa;
+	}
+
+	public String getProprietario() {
+		return proprietario;
+	}
+
+	public void setProprietario(String proprietario) {
+		this.proprietario = proprietario;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getPlaca() {
+		return placa;
+	}
+
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cpf == null) ? 0 : cpf.hashCode());
+		result = prime * result + ((placa == null) ? 0 : placa.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (cpf == null) {
+			if (other.cpf != null)
+				return false;
+		} else if (!cpf.equals(other.cpf))
+			return false;
+		if (placa == null) {
+			if (other.placa != null)
+				return false;
+		} else if (!placa.equals(other.placa))
+			return false;
+		return true;
+	}
+	
+}
---
+++ src/main/java/com/desafiobackendjr/desafio/resources/VeiculoResource.java
@@ -0,0 +1,19 @@
+package com.desafiobackendjr.desafio.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.desafiobackendjr.desafio.entities.Veiculo;
+
+@RestController
+@RequestMapping(value = "/veiculos")
+public class VeiculoResource {
+
+	@GetMapping
+	public ResponseEntity<Veiculo> findAll(){
+		Veiculo v = new Veiculo("José da Silva", "203.397.390-53", "ABC1234");
+		return ResponseEntity.ok().body(v);
+	}
+}
